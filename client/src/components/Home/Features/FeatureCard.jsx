6ae79f4e882c8ace8322e18503f88a29636fa177
Yugal Khanter <yugalkhanter82@gmail.com>
2024-01-05 19:20:40 +0530

    update: prettier configuration & file changes according to prettier config

--- client/src/components/Home/Features/FeatureCard.jsx
+++ client/src/components/Home/Features/FeatureCard.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const FeatureCard = ({ header, description }) => {
-    return (
-        <div className='sm:min-h-48 bg-white text-gray-900 py-6 px-4 border border-gray-900 rounded-3xl text-center'>
-            <span className='w-full font-semibold'>{header}</span>
-            <p className='mt-4'>{description}</p>
-        </div>
-    )
-}
+  return (
+    <div className="sm:min-h-48 bg-white text-gray-900 py-6 px-4 border border-gray-900 rounded-3xl text-center">
+      <span className="w-full font-semibold">{header}</span>
+      <p className="mt-4">{description}</p>
+    </div>
+  );
+};
 
 export default FeatureCard;
