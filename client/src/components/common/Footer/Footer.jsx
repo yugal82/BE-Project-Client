5e9d835e716a9d7ab863f26c97522e61b8835afc
GHOSTYOP <chaitanya3102@gmail.com>
2024-01-11 11:50:06 +0530

    minor tweaks

--- client/src/components/common/Footer/Footer.jsx
+++ client/src/components/common/Footer/Footer.jsx
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className="bg-gray-800 p-16 text-white">
-      <div className="flex flex-col md:flex-row items-center justify-between">
+    <div className="bg-gray-800 rounded-t-2xl p-16 text-white">
+      <div className="flex flex-col md:flex-row justify-between items-center">
         <div className="text-3xl md:text-4xl font-bold mb-6 md:mb-0">DeKrypt</div>
         <div className="flex space-x-10 items-center">
           <FaXTwitter className="text-2xl md:text-3xl hover:text-blue-400 transition duration-300 cursor-pointer transform hover:scale-110" />
@@ -15,7 +15,7 @@ const Footer = () => {
           <FaInstagram className="text-2xl md:text-3xl hover:text-blue-400 transition duration-300 cursor-pointer transform hover:scale-110" />
         </div>
       </div>
-      <div className="mt-6 text-base md:text-lg">
+      <div className="mt-6 text-base md:text-lg text-center md:text-left">
         <p>&copy; 2024 All rights reserved.</p>
         <p>
           This project is fueled by{' '}
@@ -24,23 +24,25 @@ const Footer = () => {
           </a>
         </p>
       </div>
-      <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 text-base md:text-lg mt-6 justify-center">
-        <li>
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 text-base md:text-lg mt-6 justify-center md:justify-end">
+        <div className="md:text-right">
           <Link to="/" className="hover:text-blue-400 transition duration-300">
             Home
           </Link>
-        </li>
-        <li>
-          <Link to="/nft-marketplace" className="hover:text-blue-400 transition duration-300">
-            Explore NFTs
-          </Link>
-        </li>
-        <li>
-          <Link to="/Crowdfunding" className="hover:text-blue-400 transition duration-300">
-            CrowdFunding
-          </Link>
-        </li>
-      </ul>
+        </div>
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 text-base md:text-lg mt-6 justify-center md:justify-end">
+          <div className="md:text-right">
+            <Link to="/nft-marketplace" className="hover:text-blue-400 transition duration-300">
+              Explore NFTs
+            </Link>
+          </div>
+          <div className="md:text-right">
+            <Link to="/Crowdfunding" className="hover:text-blue-400 transition duration-300">
+              CrowdFunding
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
