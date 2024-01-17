295d452f7a8106f77bc9df487d468f18e15eb089
Yugal Khanter <yugalkhanter82@gmail.com>
2024-01-17 19:04:20 +0530

    update: nft-details full layout done, added properties and token details accordian

--- client/src/components/NFT-Marketplace/nft-details/NFTDetails.jsx
+++ client/src/components/NFT-Marketplace/nft-details/NFTDetails.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import NFTNavbar from '../nft-home/NFTNavbar';
 import Footer from '../../common/Footer/Footer';
-import { FaShareNodes } from 'react-icons/fa6';
+import { IoShareOutline } from 'react-icons/io5';
+import NFTPropertiesDisclosure from './NFTPropertiesDisclosure';
+import NFTDetailsDisclosure from './NFTDetailsDisclosure';
 
 const NFTDetails = () => {
   return (
@@ -19,18 +21,18 @@ const NFTDetails = () => {
           <div className="w-full p-4">
             <div className="w-full flex items-center justify-between">
               <div>
-                <p className="font-semibold text-lg">Collection Name</p>
+                <p className="font-semibold text-lg">DeKrypt Default Collection</p>
                 <span className="text-gray-500 text-sm block">Floor price(temporary)</span>
               </div>
               <button className="flex items-center justify-center p-2 bg-gray-800 rounded-lg cursor-pointer">
-                <FaShareNodes className="w-6 h-6" />
+                <IoShareOutline className="w-6 h-6" />
                 <span className="ml-1">Share</span>
               </button>
             </div>
             <div className="mt-4 border-b-2 border-gray-800">
               <h2 className="py-2 text-3xl font-bold">Bored Ape Yatch Club #2124</h2>
               <p className="text-medium text-gray-500">
-                Owned by: <span className="text-blue-900 cursor-pointer">0xasfa.....45av</span>
+                Owned by: <span className="text-[#1d4ed8] cursor-pointer">0xasfa.....45av</span>
               </p>
               <p className="py-2">
                 Description - Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero mollitia rem ratione
@@ -40,12 +42,18 @@ const NFTDetails = () => {
             </div>
             <div className="py-8 px-4 flex items-center justify-between border-b-2 border-gray-800">
               <div className="">
-                <span className="text-medium text-gray-500">Price</span>
+                <span className="text-medium text-gray-400">Price</span>
                 <p className="text-3xl font-semibold">10 ETH</p>
               </div>
               <div className="">
                 <button className="py-3 px-24 bg-[#1d4ed8] font-semibold text-lg rounded-lg">Buy Now</button>
               </div>
+            </div>
+            <div className="py-4">
+              <NFTPropertiesDisclosure />
+            </div>
+            <div className="">
+              <NFTDetailsDisclosure />
             </div>
           </div>
         </div>
---
+++ client/src/components/NFT-Marketplace/nft-details/NFTDetailsDisclosure.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Disclosure, Transition } from '@headlessui/react';
+import { FaChevronDown } from 'react-icons/fa6';
+import { IoInformationCircle } from 'react-icons/io5';
+
+const NFTDetailsDisclosure = () => {
+  return (
+    <div className="w-full">
+      <div className="w-full rounded-2xl bg-gray-800 p-2">
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg px-4 py-2 text-left text-sm font-medium focus:outline-none focus-visible:ring">
+                <div className="flex items-center">
+                  <IoInformationCircle className="w-5 h-5" />
+                  <span className="font-semibold ml-2">Token Details</span>
+                </div>
+                <FaChevronDown className={`${open ? 'rotate-180 transform' : ''} h-5 w-5 text-white`} />
+              </Disclosure.Button>
+              <Transition
+                enter="transition duration-200 ease-linear"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-200 ease-linear"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-95 opacity-0"
+              >
+                <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-400">
+                  <div>
+                    <div className="flex items-center justify-between py-1">
+                      <p className="text-medium font-semibold text-white">Token Id</p>
+                      <p>2124</p>
+                    </div>
+                    <div className="flex items-center justify-between py-1">
+                      <p className="text-medium font-semibold text-white">Blockchain</p>
+                      <p>Ethereum</p>
+                    </div>
+                    <div className="flex items-center justify-between py-1">
+                      <p className="text-medium font-semibold text-white">Token Standard</p>
+                      <p>ERC721</p>
+                    </div>
+                    <div className="flex items-center justify-between py-1">
+                      <p className="text-medium font-semibold text-white">Contract Address</p>
+                      <p>0xca2....52rd1</p>
+                    </div>
+                  </div>
+                </Disclosure.Panel>
+              </Transition>
+            </>
+          )}
+        </Disclosure>
+      </div>
+    </div>
+  );
+};
+
+export default NFTDetailsDisclosure;
