8c4267bde8df9beac2cf7e12b23bee99b3e0af56
Atharva Hinge <atharva.3936@gmail.com>
2024-01-17 12:29:52 +0530

    changed feature card headings, added images,  general css changes

--- client/src/components/Home/Features/FeatureCard.jsx
+++ client/src/components/Home/Features/FeatureCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const FeatureCard = ({ header, description }) => {
+const FeatureCard = ({ header, description, imageURL }) => {
   return (
-    <div className="sm:min-h-48 bg-white text-gray-900 py-6 px-4 border border-gray-900 hover:scale-105 duration-100 rounded-2xl text-center">
+    <div className="  bg-white flex flex-col justify-center scale-90 items-center  text-gray-900 py-8 px-12 border border-gray-900 hover:scale-95 duration-100 rounded-2xl text-center">
+      <img src={imageURL} className="scale-50" alt="" />
       <span className="w-full font-semibold">{header}</span>
-      <p className="mt-4">{description}</p>
+      <p className="mt-2">{description}</p>
     </div>
   );
 };
---
+++ client/src/imageAssets/index.js
@@ -0,0 +1,5 @@
+import ConversationIMG from './conversation.png';
+import Scalable from './scalable.png';
+import NoAds from './noads.png';
+
+export { ConversationIMG, Scalable, NoAds };
