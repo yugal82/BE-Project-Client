a41c543d78a84e0a78d4247dd8fd0e8b27688a94
Yugal Khanter <yugalkhanter82@gmail.com>
2024-02-11 23:29:38 +0530

    update: changes in smart contract

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const NFTMarketplaceContractAddress = '0xEFfFAe49FCF8ebE44466550704927489d7ffdA46';
+export const NFTMarketplaceContractAddress = '0xa9735eadf746c6cb4586a0965fc018c66e6949c2';
 export const CrowdfundingContractAddress = '';
