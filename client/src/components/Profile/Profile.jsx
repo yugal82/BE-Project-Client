f51271e6a8a3a203c0e07070773ddc9aab308663
Yugal Khanter <yugalkhanter82@gmail.com>
2024-02-17 21:14:24 +0530

    feature: fetched listed tokens of user.

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -7,11 +7,12 @@ const Profile = () => {
   const address = useAddress();
   const [isLoading, setIsLoading] = useState(false);
 
-  const { getUserNfts, userCreatedNfts, isUserNFTsFetched } = useStateContext();
+  const { getUserNfts, userCreatedNfts, isUserNFTsFetched, getUserListedNfts } = useStateContext();
 
   const fetchUserNfts = async () => {
     setIsLoading(true);
     await getUserNfts();
+    await getUserListedNfts();
     setIsLoading(false);
   };
 
