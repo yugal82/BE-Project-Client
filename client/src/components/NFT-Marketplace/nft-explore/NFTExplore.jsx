a794d5fd4d2b0473915531bdd7d9a59efd46a357
Yugal Khanter <yugalkhanter82@gmail.com>
2024-02-19 00:47:14 +0530

    feature: fetched market listed items

--- client/src/components/NFT-Marketplace/nft-explore/NFTExplore.jsx
+++ client/src/components/NFT-Marketplace/nft-explore/NFTExplore.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAddress } from '@thirdweb-dev/react';
 import ConnectWalletPopup from '../../common/popup/ConnectWalletPopup';
 import NFTNavbar from '../nft-home/NFTNavbar';
 import Footer from '../../common/Footer/Footer';
 import NFTCard from '../../common/NFTCard';
-
-const nftCards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import { useStateContext } from '../../../context';
+import { Skeleton } from '../../common/Skeleton';
 
 const NFTExplore = () => {
   const address = useAddress();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { getListedNftsOfMarketplace, marketListedTokens, isMarketItemsFetched } = useStateContext();
+
+  const fetchMarketItems = async () => {
+    setIsLoading(true);
+    await getListedNftsOfMarketplace();
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (!isMarketItemsFetched) fetchMarketItems();
+  }, []);
+
   return (
     <div>
       {!address && <ConnectWalletPopup />}
@@ -17,11 +31,18 @@ const NFTExplore = () => {
         <div className="">
           <h2 className="text-3xl font-semibold">Explore NFTs</h2>
         </div>
-        <div className="w-full grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
-          {/* display all nft cards here! */}
-          {nftCards?.map((number) => (
-            <NFTCard key={number} number={number} />
-          ))}
+        <div>
+          {isLoading ? (
+            <div className="mt-10 grid grid-cols-1 gap-y-6 sm:gap-y-8 sm:grid-cols-2 gap-x-6 lg:grid-cols-4">
+              {Skeleton(4)}
+            </div>
+          ) : (
+            <div className="w-full grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+              {marketListedTokens?.map((nft) => (
+                <NFTCard key={nft?.tokenId} nft={nft} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
       <Footer />
