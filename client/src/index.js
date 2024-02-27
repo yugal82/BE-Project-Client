4e5b98ac9e43c03432ac77298980df8082dd3a31
Yugal Khanter <yugalkhanter82@gmail.com>
2024-02-27 12:08:20 +0530

    removed web vitals file

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import { Sepolia } from '@thirdweb-dev/chains';
 import { ThirdwebSDK } from '@thirdweb-dev/sdk';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -22,8 +21,3 @@ root.render(
     </ThirdwebProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
